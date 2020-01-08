952fa57e79680cff704965d6caaf4aa9f41c7d6f
Zach Weber <weberzt@gmail.com>
2020-01-07 18:37:13 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,53 @@
+class Dog
+
+    # def initialize(name, breed)
+    #     @name = name
+    #     @breed = breed
+    # end
+
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+end
+
+# fido = Dog.new("Fido", "Lab")
+
+
+# class Dog
+
+#     attr_accessor :name, :breed
+
+#     def initialize(name, breed)
+#         @name = name
+#         @breed = breed
+#     end
+# end
+
+# fido = Dog.new("Fido", "Lab")
+
+
+
+# class User
+
+#     # def initialize(name, email, phone)
+#     #     @name = name
+#     #     @email = email
+#     #     @phone = phone
+#     # end
+
+
+# end
+
+# user
